0820655b236b62ba65c592d3a4f80dfbd34bcfcb
DrPro6757 <43915925+DrPro6757@users.noreply.github.com>
2024-10-24 18:36:46 +0500

    Create mail.js

---
+++ Api/utils/mail.js
@@ -0,0 +1,12 @@
+        // exports.generateOTP=()=>{
+        //     let otp = ''
+        //     for (let i = 0; i <= 3; i++){
+        //         const randVal = Math.round(Math.random() * 9)
+        //         otp = otp + randVal
+        //     }
+        //     return otp 
+        // }
+
+        // // export generateOTP;
+
+        
