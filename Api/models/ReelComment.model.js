72f41fe1b878c68fb00168af2380b86f2ed0d03c
DrPro6757 <43915925+DrPro6757@users.noreply.github.com>
2024-10-24 15:46:47 +0500

    Update ReelComment.model.js

--- Api/models/ReelComment.model.js
+++ Api/models/ReelComment.model.js
@@ -1,28 +1,27 @@
 const mongoose = require('mongoose');
 
-const ReelCommentsSchema = mongoose.Schema({
-    comment:{
-        type:String,
-        max:1000
+const ReelCommentsSchema = mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      max: 1000,
     },
-    username:{
-        type:String,
-        required:true
+    username: {
+      type: String,
     },
-    userId:{
-        type:String,
-        required:true
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Users',
     },
-    postId:{
-        type:String,
-        required:true
-    }
-   
-
-},
-{timestamps:true}
+    postId: {
+      type: String,
+      required: true,
+    },
+  },
+  {timestamps: true},
 );
 
-const ReelComments = mongoose.model("Reels_Comments",ReelCommentsSchema);
+const ReelComments = mongoose.model('Reels_Comments', ReelCommentsSchema);
 
 module.exports = ReelComments;
