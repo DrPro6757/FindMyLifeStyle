a4026b579fda1021698c28912d4cf9e6bb2cbae8
DrPro6757 <43915925+DrPro6757@users.noreply.github.com>
2024-10-24 15:41:29 +0500

    Update PostEvent.model.js

--- Api/models/PostEvent.model.js
+++ Api/models/PostEvent.model.js
@@ -1,177 +1,229 @@
 const mongoose = require('mongoose');
 
-const PostEventSchema = mongoose.Schema({
-    eventName:{
-        type:String,
-        max:170,
-        required:true
+const PostEventSchema = mongoose.Schema(
+  {
+    eventName: {
+      type: String,
+      max: 170,
+      required: true,
     },
-    caption:{
-        type:String,
-        max:200
+    caption: {
+      type: String,
+      max: 200,
     },
-    username:{
-        type:String,
-        // required:true
+    username: {
+      type: String,
+      //   required: true,
     },
-    userCoverPic:{
-        type:String,
+    userCoverPic: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Users',
+    },
+    profilePic: {
+      type: String,
+    },
+    imageUrl: {
+      type: Array,
+      default: [],
+      required: true,
+      // type:{type:String,required:true},
+      // file:[]
+    },
+    location: {
+      type: {type: String, required: true},
+      coordinates: [],
+    },
+    postLikes: {
+      type: Array,
+      default: [],
+    },
+    postComments: {
+      type: Array,
+      default: [],
     },
 
-    userId:{
-        type:String,
-        required:true
+    postShares: {
+      type: Array,
+      default: [],
     },
-    profilePic:{
-        type:String,
+    caption: {
+      type: String,
     },
-    imageUrl:{
-        type : Array,
-        default:[],
-        required : true
-        // type:{type:String,required:true},
-        // file:[]
+    email: {
+      type: String,
     },
-    location:{
-        type:{type:String,required:true},
-       coordinates:[],
+    eventCategory: {
+      type: String,
+      default: '',
     },
-    postLikes:{
-        type:Array,
-        default:[],
+    ticketFreeAmenities: {
+      type: String,
+      default: '',
     },
-    postComments:{
-        type:Array,
-        default:[],
+    ticket1Amenities: {
+      type: String,
+      default: '',
     },
-    
-    postShares:{
-        type:Array,
-        default:[],
+    ticket2Amenities: {
+      type: String,
+      default: '',
     },
-    caption:{
-        type:String,
+    ticket3Amenities: {
+      type: String,
+      default: '',
     },
-    email:{
-        type:String,
+    ticket4Amenities: {
+      type: String,
+      default: '',
     },
-    ticketFreeAmenities:{
-        type:String,
+    ticket5Amenities: {
+      type: String,
+      default: '',
     },
-    ticket1Amenities:{
-        type:String,
+    ticket1Price: {
+      type: String,
+      default: '',
     },
-    ticket2Amenities:{
-        type:String,
+    ticket2Price: {
+      type: String,
+      default: '',
     },
-    ticket3Amenities:{
-        type:String,
+    ticket3Price: {
+      type: String,
+      default: '',
     },
-    ticket4Amenities:{
-        type:String,
+    ticket4Price: {
+      type: String,
+      default: '',
     },
-    ticket5Amenities:{
-        type:String,
+    ticket5Price: {
+      type: String,
+      default: '',
     },
-    ticket1Prices:{
-        type:String,
+    ticket1Buyer: {
+      type: Array,
+      default: [],
     },
-    ticket2Prices:{
-        type:String,
+    ticket2Buyer: {
+      type: Array,
+      default: [],
     },
-    ticket3Prices:{
-        type:String,
+    ticket3Buyer: {
+      type: Array,
+      default: [],
     },
-    ticket4Prices:{
-        type:String,
+    ticket4Buyer: {
+      type: Array,
+      default: [],
     },
-    ticket5Prices:{
-        type:String,
+    ticket5Buyer: {
+      type: Array,
+      default: [],
     },
-    eventType:{
-        type:String,
+    eventType: {
+      type: String,
+      default: '',
     },
-    viewPermission:{
-        type: Array,
-        default:[]
+    viewPermission: {
+      type: Array,
+      default: [],
     },
-    eventCategory:{
-        type:String,
-    },
-    importantNote:{
-        type:String,
-        max:200
-    },
-    eventStartDate:{
-        type:String,
-    },
-    eventDate:{
-        type:String,
-    },
-    eventEndDate:{
-        type:String,
-    },
-    eventDays:{
-        type:String,
-    },
-    eventTime:{
-        type:String,
-        max:200
-    },
-    eventStartTime:{
-        type:String,
-        max:200
-    },
-    eventEndTime:{
-        type:String,
-        max:200
-    },
-    eventDescription:{
-        type:String,
-    },
-    eventJoinRequests:{
-        type:Array,
-        default:[],
-    },
-    eventMembersList:{
-        type:Array,
-        default:[],
-    },
-    gender:{
-        type:String,
-    },
-    interest:{
-        type:String,
-    },
-    level:{
-        type:String,
-    },
-    rank:{
-        type:Number,
-    },
-    members:{
-        type:Number,
-    },
-    postTimeData:{
-        type:String,
-    },
-    userImage:{
-        type:String,
-    },
-    status:{
-        type:String,
-    },
-    fcmTokens:{
-        type:Array,
-        default:[],
-    }
-   
 
-},
-{timestamps:true}
+    importantNote: {
+      type: String,
+      max: 200,
+      default: '',
+    },
+    eventStartDate: {
+      type: String,
+      default: '',
+    },
+    eventDate: {
+      type: String,
+      default: '',
+    },
+    eventEndDate: {
+      type: String,
+      default: '',
+    },
+    eventDays: {
+      type: String,
+      default: '',
+    },
+    eventTime: {
+      type: String,
+      max: 200,
+      default: '',
+    },
+    eventStartTime: {
+      type: String,
+      max: 200,
+      default: '',
+    },
+    eventEndTime: {
+      type: String,
+      max: 200,
+      default: '',
+    },
+    eventDescription: {
+      type: String,
+      default: '',
+    },
+    eventJoinRequests: {
+      type: Array,
+      default: [],
+    },
+    eventMembersList: {
+      type: Array,
+      default: [],
+    },
+    gender: {
+      type: String,
+      default: '',
+    },
+    interest: {
+      type: String,
+      default: '',
+    },
+    level: {
+      type: String,
+      default: '',
+    },
+    rank: {
+      type: Number,
+      default: 0,
+    },
+    members: {
+      type: Number,
+      default: 0,
+    },
+    postTimeData: {
+      type: String,
+      default: '',
+    },
+    userImage: {
+      type: String,
+      default: '',
+    },
+    status: {
+      type: String,
+      default: '',
+    },
+    fcmTokens: {
+      type: Array,
+      default: [],
+    },
+  },
+  {timestamps: true},
 );
-PostEventSchema.index({location:"2dsphere"})
-const PostEvent = mongoose.model("Posts_Events",PostEventSchema);
+PostEventSchema.index({location: '2dsphere'});
+// PostEventSchema.index({ticket1Buyer:"2dsphere"})
+
+const PostEvent = mongoose.model('Posts_Events', PostEventSchema);
 
 module.exports = PostEvent;
 
