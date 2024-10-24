b4a7bd1b0bdf6bffab1e98c2b876f1225974b4d2
DrPro6757 <43915925+DrPro6757@users.noreply.github.com>
2024-10-24 15:45:05 +0500

    Update PostUser.model.js

--- Api/models/PostUser.model.js
+++ Api/models/PostUser.model.js
@@ -1,75 +1,74 @@
 const mongoose = require('mongoose');
 
-const PostUserSchema = mongoose.Schema({
-    caption:{
-        type:String,
-        max:200
+const PostUserSchema = mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      max: 200,
     },
-    username:{
-        type:String,
-        required:true
-    },
-
-    userId:{
-        type:String,
-        required:true
-    },
-    imageUrl:{
-        type:String,
-    },
-    videoUrl:{
-        type:String,
-    },
-    reelUrl:{
-        type:String,
-    },
-    location:{
-        type:{type:String},
-       coordinates:[],
-    },
-    profilePic:{
-        type:String,
+    username: {
+      type: String,
     },
 
-    postLikes:{
-        type:Array,
-        default:[],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Users',
     },
-    postComments:{
-        type:Array,
-        default:[],
+    imageUrl: {
+      type: String,
     },
-    
-    postShares:{
-        type:Array,
-        default:[],
+    videoUrl: {
+      type: String,
     },
-    email:{
-        type:String,
+    reelUrl: {
+      type: String,
     },
-    postDescription:{
-        type:String,
+    location: {
+      type: {type: String},
+      coordinates: [],
     },
-    interest:{
-        type:String,
+    profilePic: {
+      type: String,
     },
-    userImage:{
-        type:String,
-    },
-    status:{
-        type:String,
-    },
-    fcmTokens:{
-        type:Array,
-        default:[],
-    }
-   
 
-},
-{timestamps:true}
+    postLikes: {
+      type: Array,
+      default: [],
+    },
+    postComments: {
+      type: Array,
+      default: [],
+    },
+
+    postShares: {
+      type: Array,
+      default: [],
+    },
+    email: {
+      type: String,
+    },
+    postDescription: {
+      type: String,
+    },
+    interest: {
+      type: String,
+    },
+    userImage: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+    fcmTokens: {
+      type: Array,
+      default: [],
+    },
+  },
+  {timestamps: true},
 );
-PostUserSchema.index({location:"2dsphere"})
-const PostUser = mongoose.model("Posts_Users",PostUserSchema);
+PostUserSchema.index({location: '2dsphere'});
+const PostUser = mongoose.model('Posts_Users', PostUserSchema);
 
 module.exports = PostUser;
 
